217fec4ecdd2824b7d5e5a712e785acd9c3ba98a
YourtionGuo <yourtion@gmail.com>
2017-02-21 12:00:22 +0800

    api view

--- src/views/API.vue
+++ src/views/API.vue
@@ -3,7 +3,29 @@
  <h2> {{ group_name }}</h2>
  <h3>项目：{{ $route.params.project }} 文件：{{ $route.params.file }}</h3>
  <div>
-  {{ content }}
+  <el-collapse v-model="activeName" accordion>
+    <el-collapse-item v-for="item in content.apis" :name="item.title">
+    <template slot="title">
+      <el-tag type="primary">{{ item. method }}</el-tag> {{ item.url }} - {{ item.title }}
+    </template>
+      <el-row :gutter="20">
+        <el-col :span="16">
+          <el-table :data="item.params" style="width: 100%">
+            <el-table-column prop="name" label="参数明" width="180"> </el-table-column>
+            <el-table-column label="内容" width="400"> 
+              <template scope="scope"><el-input size="small" :placeholder="scope.row.name"></el-input></template>
+            </el-table-column>
+            <el-table-column prop="type" label="类型" width="150"> </el-table-column>
+            <el-table-column label="必填" width="100"> 
+              <template scope="scope">{{ scope.row.required ? '是' : '否' }}</template>
+            </el-table-column>
+            <el-table-column prop="description" label="描述"> </el-table-column>
+          </el-table>
+        </el-col>
+        <el-col :span="8"><pre> {{ item.result }} </pre></el-col>
+      </el-row>
+    </el-collapse-item>
+  </el-collapse>
  </div>
  </div>
 </template>
@@ -11,16 +33,18 @@
   export default {
     created() {
       this.loading = true;
-      console.log(this.$route.params);
       this.$store.dispatch('getApiFile', this.$route.params).then(() => { this.loading = false; });
     },
     data() {
       return {
         loading: true,
-        content: this.$store.state.content,
+        activeName: '',
       };
     },
     computed: {
+      content() {
+        return this.$store.state.content;
+      },
       group_name() {
         return this.$store.state.content.group;
       },
