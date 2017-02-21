52ec7b4fc15751f92fc2499264941d2ce79903fc
YourtionGuo <yourtion@gmail.com>
2017-02-21 16:29:03 +0800

    list API

---
+++ src/components/ApiContent.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="16">
+      <el-form ref="form" :model="form" :rules="rules">
+        <el-table :data="item.params" style="width: 100%">
+          <el-table-column prop="name" label="参数" width="180"> </el-table-column>
+          <el-table-column label="内容" width="400"> 
+            <template scope="scope">
+              <el-form-item :prop="scope.row.name">
+                <el-input v-model="form[scope.row.name]" size="small" :placeholder="scope.row.name"></el-input>
+              </el-form-item>
+            </template>
+          </el-table-column>
+          <el-table-column prop="type" label="类型" width="150"> </el-table-column>
+          <el-table-column label="必填" width="100"> 
+            <template scope="scope">{{ scope.row.required ? '是' : '否' }}</template>
+          </el-table-column>
+          <el-table-column prop="description" label="描述"> </el-table-column>
+        </el-table>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">测试API</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+    <el-col :span="8"><pre> {{ item.result }} </pre></el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  props: [ 'item' ],
+  data() {
+    return {
+      form: this.getForm(),
+      rules: this.getRules(),
+    };
+  },
+  methods: {
+    getForm() {
+      const res = {};
+      for(const item of this.item.params) {
+        res[item.name] = '';
+      }
+      return res;
+    },
+    getRules() {
+      const res = {};
+      for(const item of this.item.params) {
+        res[item.name] = [{ required: item.required, message: `请输入 ${ item.name }`, trigger: 'blur' }];
+      }
+      return res;
+    },
+    onSubmit() {
+      console.log(this.form);
+    },
+  },
+};
+</script>
+<style>
+</style>
--- src/views/API.vue
+++ src/views/API.vue
@@ -8,48 +8,37 @@
     <template slot="title">
       <el-tag type="primary">{{ item. method }}</el-tag> {{ item.url }} - {{ item.title }}
     </template>
-      <el-row :gutter="20">
-        <el-col :span="16">
-          <el-table :data="item.params" style="width: 100%">
-            <el-table-column prop="name" label="参数明" width="180"> </el-table-column>
-            <el-table-column label="内容" width="400"> 
-              <template scope="scope"><el-input size="small" :placeholder="scope.row.name"></el-input></template>
-            </el-table-column>
-            <el-table-column prop="type" label="类型" width="150"> </el-table-column>
-            <el-table-column label="必填" width="100"> 
-              <template scope="scope">{{ scope.row.required ? '是' : '否' }}</template>
-            </el-table-column>
-            <el-table-column prop="description" label="描述"> </el-table-column>
-          </el-table>
-        </el-col>
-        <el-col :span="8"><pre> {{ item.result }} </pre></el-col>
-      </el-row>
+      <api-content :item="item"></api-content>
     </el-collapse-item>
   </el-collapse>
  </div>
  </div>
 </template>
 <script>
-  export default {
-    created() {
-      this.loading = true;
-      this.$store.dispatch('getApiFile', this.$route.params).then(() => { this.loading = false; });
+import ApiContent from 'components/ApiContent.vue';
+export default {
+  components: {
+    'api-content': ApiContent,
+  },
+  created() {
+    this.loading = true;
+    this.$store.dispatch('getApiFile', this.$route.params).then(() => { this.loading = false; });
+  },
+  data() {
+    return {
+      loading: true,
+      activeName: '',
+    };
+  },
+  computed: {
+    content() {
+      return this.$store.state.content;
     },
-    data() {
-      return {
-        loading: true,
-        activeName: '',
-      };
+    group_name() {
+      return this.$store.state.content.group;
     },
-    computed: {
-      content() {
-        return this.$store.state.content;
-      },
-      group_name() {
-        return this.$store.state.content.group;
-      },
-    },
-  };
+  },
+};
 </script>
 <style>
   .index {
