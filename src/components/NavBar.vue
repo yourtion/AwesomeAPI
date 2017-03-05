9ea1e8807605c36db3b6d898ee5cd542f4ed8063
YourtionGuo <yourtion@gmail.com>
2017-03-05 15:21:44 +0800

    update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <el-menu router theme="dark" default-active="1" mode="horizontal">
     <el-menu-item index="/home">API管理系统 - {{ project }}</el-menu-item>
-    <el-menu-item v-for="item in list" :index="'/api/' + project + '/' + item">{{ item }}</el-menu-item>
+    <el-menu-item v-for="item in list" :index="`/api/${project}/${item}`" :key="`${project}_${item}`">{{ item }}</el-menu-item>
   </el-menu>
 </template>
 
